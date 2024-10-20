5af7c23e3967d17881cf38e842aa7ddde1ba43ec
TrymbakMahant <trymbak0102mahant@gmail.com>
2024-10-19 21:42:58 -0700

    console.log use client

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
-
+"use client"
 import styles from "./page.module.css";
 import {  initData} from '@telegram-apps/sdk-react';
 
 
 export default function Home() {
   const ll = initData
-  console.log(ll.authDate ,ll.canSendAfter , ll.chat , ll.chatInstance , ll.hash , ll.raw)
-  console.log(initData)
+  console.log(ll.authDate() ,ll.canSendAfter() , ll.chat() , ll.chatInstance() , ll.hash() , ll.raw())
+
   return (
     <div className={styles.page}>
+     <button onClick={()=>{
+      console.log("hello",ll)
+     }}>check</button>
       hello
     </div>
   );
